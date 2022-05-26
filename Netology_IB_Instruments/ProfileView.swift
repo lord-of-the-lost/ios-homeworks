b6e66bf394f03fe3cb4f969e36e95b777cc59063
Ignatov <ignatov.99@yandex.ru>
2022-05-26 12:27:55 +0300

    add constraints

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -8,5 +8,12 @@
 import UIKit
 
 class ProfileView: UIView {
+    
+    @IBOutlet weak var userPhoto: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userAge: UILabel!
+    @IBOutlet weak var userCity: UILabel!
+    @IBOutlet weak var someText: UITextView!
+    
 
 }
--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -11,8 +11,11 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let screenWidth = UIScreen.main.bounds.width
+        if let myView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            myView.frame = CGRect(x: 16, y: 50, width: screenWidth - 32 , height: 400)
+            view.addSubview(myView)
+        }
     }
     
 
